6d0580cab99e3e5489a8a9b900d07009085382e9
Antonio Quevedo <thecreed32@hotmail.com>
2021-08-18 23:46:20 -0500

    19/09/2021

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+   $('#two').poptrox({
+   	popupCloserBackgroundColor:	'#BBB59B',
+   	popupPadding: 0,	
+   	overlayColor: '#2c2c2c',
+		overlayOpacity: 0.85,
+		popupCloserText: '×',
+		popupLoaderText: '***',
+		selector: 'a',
+		usePopupCaption: false,
+		usePopupEasyClose: false,
+		usePopupNav: true,
+		windowMargin: 150,	
+		popupCloserTextColor: ' #3D3A33',	
+		popupCloserTextSize: '30px',
+   });
+			
+});
